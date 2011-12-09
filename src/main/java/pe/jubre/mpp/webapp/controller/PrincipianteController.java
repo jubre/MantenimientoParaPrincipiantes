fcd946ead307facce6692523e9516884c45269de
Junior Corman <junior.corman@gmail.com>
2011-12-09 08:41:06 -0500

    update

--- src/main/java/pe/jubre/mpp/webapp/controller/PrincipianteController.java
+++ src/main/java/pe/jubre/mpp/webapp/controller/PrincipianteController.java
@@ -1,6 +1,7 @@
 package pe.jubre.mpp.webapp.controller;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.ServletRequestBindingException;
