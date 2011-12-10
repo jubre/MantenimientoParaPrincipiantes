d6134c03b493720a484ccb0bd8b3bfae62cde9b4
Junior Corman <junior.corman@gmail.com>
2011-12-10 00:30:48 -0500

    update

--- src/main/resources/mpp/testdb/schema.sql
+++ src/main/resources/mpp/testdb/schema.sql
@@ -1,3 +1,7 @@
 drop table PRINCIPIANTE if exists;
 
-create table PRINCIPIANTE (PRINCIPIANTE_ID integer identity primary key, NOMBRE varchar(50) not null, FECHA_CREACION date);
+create table PRINCIPIANTE (PRINCIPIANTE_ID integer identity primary key, 
+	NOMBRE varchar(50) not null,
+	APELLIDO_PATERNO varchar(50) not null,
+	APELLIDO_MATERNO varchar(50) not null,
+	FECHA_CREACION date);
